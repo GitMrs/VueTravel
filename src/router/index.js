d946c40683ecc692e9540e7b0dfc9b642f3ec7bc
990826524@qq.com <github520>
2018-05-24 16:57:35 +0800

    优化

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Home from '@/pages/home/home'
 import City from '@/pages/city/city'
 import Detail from '@/pages/detail/detail'
 Vue.use(Router)
-
+//  异步组件的加载使用箭头函数替换原来的模板这个方式使用内部里面的组件引用 () => import('路径')
 export default new Router({
   routes: [
     {
